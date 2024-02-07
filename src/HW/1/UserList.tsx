5ba4ca0ac18806710c87d0492f86d82c67f90cce
ziplenok <shivczov00@gmail.com>
2024-02-07 17:40:59 +0600

    HW1

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,36 @@
+type AddressType = {
+  street: string;
+  city: string;
+};
+
+type UserType = {
+  id: number;
+  name: string;
+  age: number;
+  address: AddressType;
+};
+
+type UserListPropsType = {
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={"hw01-users"}>
+      <h2>User List:</h2>
+
+      <ul>
+        {props.users.map((user: UserType, index: number) => (
+          <li
+            key={props.users[index].id}
+            id={`hw01-user-${props.users[index].id}`}
+          >
+            <strong>{user.name}</strong> (Age: {user.age})
+            <strong> Address:</strong>
+            {user.address.street}, {user.address.city}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
